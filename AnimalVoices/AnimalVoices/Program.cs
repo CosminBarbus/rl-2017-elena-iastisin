e11543ddba2ea5ba832ea8e8fd0971297ffa3f11
elenaiatisin <iatisine@gmail.com>
2017-12-03 20:42:08 +0200

    Session 1

---
+++ AnimalVoices/AnimalVoices/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalVoices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal animal1 = new Animal("Cat", "meao-meao");
+            Animal animal2 = new Animal("Dog", "woof-woof");
+            Animal animal3 = new Animal("Cow", "moo-moo");
+
+            List<Animal> Animals = new List<Animal>
+            {
+                animal1,
+                animal2,
+                animal3
+            };
+
+            foreach (Animal item in Animals)
+            {
+                Console.WriteLine("{0} make {1}", item.GetAnimalType(), item.GetAnimalVoice());
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
